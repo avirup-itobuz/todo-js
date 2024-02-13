e9ed77cdf3f0bb4872d15d6c777ca8c19772d695
avirup-itobuz <avirup@itobuz.com>
2024-02-13 18:49:11 +0530

    commit

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const tasks_data = [
 
 const tasks = tasks_data.map((task) => {
   const container = document.createElement("div");
-  container.classList.add("w-100", "d-flex");
+  container.classList.add("w-100", "d-flex", "align-items-center", "fs-5");
   const taskTitle = document.createElement("div");
-  taskTitle.innerHTML = `<h3>${task.title}</h3>`;
+  taskTitle.classList.add(
+    "d-flex",
+    "justify-content-center",
+    "align-items-center"
+  );
+  taskTitle.innerHTML = `<h4>${task.title}</h4>`;
   container.appendChild(taskTitle);
+  const horizontalbar = document.createElement("hr");
   tasks_container.appendChild(container);
+  tasks_container.appendChild(horizontalbar);
 });
